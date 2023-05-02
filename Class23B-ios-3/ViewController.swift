bc877a5420cc9e430c6dc56cac662628b3253df5
LiwaaHosh <66652532+LiwaaCoder@users.noreply.github.com>
2023-05-03 01:08:43 +0300

    IOS soccer

--- Class23B-ios-3/ViewController.swift
+++ Class23B-ios-3/ViewController.swift
@@ -1,19 +1,33 @@
-//
-//  ViewController.swift
-//  Class23B-ios-3
-//
-//  Created by liwaa on 02/05/2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var labelFirst: UILabel!
+    @IBOutlet weak var labelSecond: UILabel!
+    
+    var scores = [0, 0]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func secondTeamAttack(_ sender: Any) {
+        scores[0] += 1
+        updateUI(scores)
+    }
+    
+    @IBAction func firstTeamAttack(_ sender: Any) {
+        scores[1] += 1
+        updateUI(scores)
+    }
+    
+    func updateUI(_ arr: [Int]) {
+        labelFirst.text = "Macabi \(arr[0])"
+        labelSecond.text = "Hapoel \(arr[1])"
+    }
+    
 }
 
